f5319319c76134ea04e283e6fa2fbd4ce59e06c8
Desarrollo <Desarrollo@192.168.0.100>
2025-09-17 17:36:53 +0200

    Ejemplo básico de Programación Orientada a Objetos

---
+++ oop/src/com/ipartek/formacion/pojos/Persona.java
@@ -0,0 +1,56 @@
+package com.ipartek.formacion.pojos;
+
+public class Persona {
+	// CONSTANTES
+	private static final Long ID_POR_DEFECTO = null;
+	private static final String NOMBRE_POR_DEFECTO = "ANÓNIMO";
+	
+	// VARIABLES DE INSTANCIA
+	private Long id;
+	private String nombre;
+
+	// CONSTRUCTORES
+	public Persona(Long id, String nombre) {
+		setId(id);
+		setNombre(nombre);
+	}
+	
+	public Persona(String nombre) {
+		this(ID_POR_DEFECTO, nombre);
+	}
+
+	public Persona() {
+		this(ID_POR_DEFECTO, NOMBRE_POR_DEFECTO);
+	}
+	
+	// Constructor de copia
+	public Persona(Persona persona) {
+		this(persona.getId(), persona.getNombre());
+	}
+
+	// GETTERS Y SETTERS (MÉTODOS DE ACCESO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if(nombre == null || nombre.isBlank()) {
+			throw new RuntimeException("El nombre es obligatorio");
+		}
+		
+		this.nombre = nombre.trim();
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + "]";
+	}
+}
---
+++ oop/src/com/ipartek/formacion/pojos/PersonaPrueba.java
@@ -0,0 +1,29 @@
+package com.ipartek.formacion.pojos;
+
+public class PersonaPrueba {
+	public static void main(String[] args) {
+		var persona1 = new Persona();
+		
+		System.out.println("#" + persona1.getId() + ", " + persona1.getNombre() + "#");
+		
+		persona1.setId(1L);
+		persona1.setNombre("    Javier      ");
+		
+		System.out.println(persona1.toString());
+		
+		var persona2 = new Persona(2L, "Pepe");
+		
+		System.out.println(persona2);
+
+		var persona3 = new Persona("Juan");
+		
+		System.out.println(persona3);
+		
+		var persona4 = new Persona(persona3);
+		
+		persona4.setId(5L);
+		
+		System.out.println(persona3);
+		System.out.println(persona4);
+	}
+}
