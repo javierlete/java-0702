d3c7712c422934d7a6be2c7093b16e77bd04d6d8
Desarrollo <Desarrollo@192.168.0.48>
2025-09-30 15:50:58 +0200

    Creación de DaoJdbc como biblioteca para hacer DAOs

---
+++ oop/src/com/ipartek/formacion/bibliotecas/DaoJdbc.java
@@ -0,0 +1,63 @@
+package com.ipartek.formacion.bibliotecas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.ipartek.formacion.citas.accesodatos.AccesoDatosException;
+
+public class DaoJdbc<T> {
+	private String url;
+	private String user;
+	private String pass;
+	
+	public DaoJdbc(String url, String user, String pass) {
+		super();
+		this.url = url;
+		this.user = user;
+		this.pass = pass;
+	}
+
+	public Collection<T> ejecutarConsulta(String sql, Mapeador<T> mapeador, Object... args) {
+		try (Connection con = DriverManager.getConnection(url, user, pass);
+				PreparedStatement pst = con.prepareStatement(sql);) {
+
+			int i = 1;
+
+			for (var arg : args) {
+				pst.setObject(i++, arg);
+			}
+
+			if (pst.execute()) {
+				ResultSet rs = pst.getResultSet();
+
+				ArrayList<T> objetos = new ArrayList<>();
+
+				while (rs.next()) {
+					objetos.add(mapeador.mapear(rs));
+				}
+
+				return objetos;
+			} else {
+				int numeroRegistrosModificados = pst.getUpdateCount();
+
+				if (numeroRegistrosModificados == 0) {
+					throw new AccesoDatosException("Error de concurrencia");
+				}
+
+				return null;
+			}
+
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se ha podido ejecutar la consulta", e);
+		}
+	}
+
+	public interface Mapeador<T> {
+		T mapear(ResultSet rs) throws SQLException;
+	}
+}
--- oop/src/com/ipartek/formacion/citas/accesodatos/DaoCitaJdbc.java
+++ oop/src/com/ipartek/formacion/citas/accesodatos/DaoCitaJdbc.java
@@ -1,36 +1,31 @@
 package com.ipartek.formacion.citas.accesodatos;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
+import com.ipartek.formacion.bibliotecas.DaoJdbc;
 import com.ipartek.formacion.citas.entidades.Cita;
 
 public class DaoCitaJdbc implements DaoCita {
 
-	private static final String URL = "jdbc:sqlite:bdd/citas.db";
-	private static final String USER = "";
-	private static final String PASS = "";
+	private final DaoJdbc<Cita> dao = new DaoJdbc<>("jdbc:sqlite:bdd/citas.db", "", "");
 
 	@Override
 	public Collection<Cita> obtenerTodos() {
-		return ejecutarConsulta("SELECT * FROM citas");
+		return dao.ejecutarConsulta("SELECT * FROM citas", DaoCitaJdbc::mapeador);
 	}
 
 	@Override
 	public Optional<Cita> obtenerPorId(Long id) {
-		return ejecutarConsulta("SELECT * FROM citas WHERE id=?", id).stream().findFirst();
+		return dao.ejecutarConsulta("SELECT * FROM citas WHERE id=?", DaoCitaJdbc::mapeador, id).stream().findFirst();
 	}
 
 	@Override
 	public Cita insertar(Cita cita) {
-		ejecutarConsulta("INSERT INTO citas (texto,inicio,fin) VALUES (?,?,?)", cita.getTexto(),
+		dao.ejecutarConsulta("INSERT INTO citas (texto,inicio,fin) VALUES (?,?,?)", null, cita.getTexto(),
 				Timestamp.valueOf(cita.getInicio()), Timestamp.valueOf(cita.getFin()));
 
 		return cita;
@@ -38,7 +33,7 @@ public class DaoCitaJdbc implements DaoCita {
 
 	@Override
 	public Cita modificar(Cita cita) {
-		ejecutarConsulta("UPDATE citas SET texto=?,inicio=?,fin=? WHERE id=?", cita.getTexto(),
+		dao.ejecutarConsulta("UPDATE citas SET texto=?,inicio=?,fin=? WHERE id=?", null, cita.getTexto(),
 				Timestamp.valueOf(cita.getInicio()), Timestamp.valueOf(cita.getFin()), cita.getId());
 
 		return cita;
@@ -46,48 +41,16 @@ public class DaoCitaJdbc implements DaoCita {
 
 	@Override
 	public void borrar(Long id) {
-		ejecutarConsulta("DELETE FROM citas WHERE id=?", id);
+		dao.ejecutarConsulta("DELETE FROM citas WHERE id=?", null, id);
 	}
 
 	@Override
 	public Collection<Cita> buscarPorTexto(String texto) {
-		return ejecutarConsulta("SELECT * FROM citas WHERE texto LIKE ?", "%" + texto + "%");
+		return dao.ejecutarConsulta("SELECT * FROM citas WHERE texto LIKE ?", DaoCitaJdbc::mapeador, "%" + texto + "%");
 	}
 
-	private Collection<Cita> ejecutarConsulta(String sql, Object... args) {
-		try (Connection con = DriverManager.getConnection(URL, USER, PASS);
-				PreparedStatement pst = con.prepareStatement(sql);) {
-
-			int i = 1;
-
-			for (var arg : args) {
-				pst.setObject(i++, arg);
-			}
-
-			if (pst.execute()) {
-				ResultSet rs = pst.getResultSet();
-
-				ArrayList<Cita> citas = new ArrayList<>();
-
-				while (rs.next()) {
-					citas.add(new Cita(rs.getLong("id"), rs.getString("texto"),
-							rs.getTimestamp("inicio").toLocalDateTime(), rs.getTimestamp("fin").toLocalDateTime()));
-				}
-
-				return citas;
-			} else {
-				int numeroRegistrosModificados = pst.getUpdateCount();
-
-				if (numeroRegistrosModificados == 0) {
-					throw new AccesoDatosException("Error de concurrencia");
-				}
-
-				return null;
-			}
-
-		} catch (SQLException e) {
-			throw new AccesoDatosException("No se ha podido ejecutar la consulta", e);
-		}
+	private static Cita mapeador(ResultSet rs) throws SQLException {
+		return new Cita(rs.getLong("id"), rs.getString("texto"), rs.getTimestamp("inicio").toLocalDateTime(),
+				rs.getTimestamp("fin").toLocalDateTime());
 	}
-
 }
