b7bfaa4d7fb2da845c27921f44500cb7b818ebe9
Desarrollo <Desarrollo@192.168.0.54>
2025-10-07 17:13:23 +0200

    Uso de Logger

--- taller/src/main/java/com/ipartek/formacion/bibliotecas/controladores/ControladorFrontal.java
+++ taller/src/main/java/com/ipartek/formacion/bibliotecas/controladores/ControladorFrontal.java
@@ -5,6 +5,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import io.github.classgraph.AnnotationInfo;
 import io.github.classgraph.ClassGraph;
@@ -21,43 +23,70 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ControladorFrontal extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final Logger log = Logger.getLogger(ControladorFrontal.class.getName());
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try (ScanResult scanResult = new ClassGraph().enableAllInfo().scan()) {
 			String path = request.getPathInfo();
 
+			log.log(Level.INFO, "PATHINFO: {0}", path);
+
 			for (ClassInfo classInfo : scanResult.getClassesWithMethodAnnotation(Ruta.class)) {
+				log.log(Level.FINEST, "CLASE?: {0}", classInfo.getName());
+
 				Class<?> clase = classInfo.loadClass();
 
 				for (MethodInfo methodInfo : classInfo.getMethodInfo()) {
+					log.log(Level.FINEST, "METODO?: {0}", methodInfo.getName());
+
 					AnnotationInfo anotacion = methodInfo.getAnnotationInfo(Ruta.class);
-					
+
+					log.log(Level.FINEST, "ANOTACION?: {0}", anotacion);
+
 					if (anotacion != null) {
+
 						Object valor = anotacion.getParameterValues().getValue("value");
-						
+
+						log.log(Level.FINEST, "VALOR?: {0}", valor);
+
 						if (path.equals(valor)) {
 							Method method = methodInfo.loadClassAndGetMethod();
 
 							Object instancia = clase.getDeclaredConstructor().newInstance();
 
-							System.out.println("Invocando " + clase.getName() + "." + method.getName());
-							
+							log.log(Level.INFO, "LLAMADA A METODO: {0}.{1}()", new Object[] { clase.getName(), method.getName() });
+
 							Map<String, String> entrada = new HashMap<String, String>();
 							Map<String, Object> salida = new HashMap<String, Object>();
-							
+
 							request.getParameterMap().forEach((clave, array) -> {
 								entrada.put(clave, array[0]);
 							});
-							
+
+							log.log(Level.FINE, "ENTRADA: {0}", entrada);
+
 							String vista = (String) method.invoke(instancia, entrada, salida);
-							
+
 							salida.forEach((k, v) -> request.setAttribute(k, v));
+
+							log.log(Level.FINE, "SALIDA: {0}", salida);
+							log.log(Level.FINE, "VISTA: {0}", vista);
+
+							String pathVistaAbsoluto = "/WEB-INF/vistas/" + vista + ".jsp";
 							
-							request.getRequestDispatcher("/WEB-INF/vistas/" + vista + ".jsp").forward(request, response);
+							log.log(Level.FINER, "VISTA ABSOLUTO: {0}", pathVistaAbsoluto);
+							
+							request.getRequestDispatcher(pathVistaAbsoluto).forward(request,
+									response);
+
+							return;
 						}
 					}
 				}
 			}
+
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
 				| NoSuchMethodException | SecurityException e) {
 			e.printStackTrace();
@@ -70,5 +99,5 @@ public class ControladorFrontal extends HttpServlet {
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
+
 }
