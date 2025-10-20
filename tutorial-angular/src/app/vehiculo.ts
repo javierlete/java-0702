9c52ec4489d8f9d9b16a69095a265d42de6e1e3d
Javier Lete <javier@eskura21.com>
2025-10-20 17:24:09 +0200

    Vehiculo ahora tiene EstadoReparacion

--- tutorial-angular/src/app/vehiculo.ts
+++ tutorial-angular/src/app/vehiculo.ts
@@ -1,7 +1,10 @@
+import { EstadoReparacion } from "./estado-reparacion";
+
 export interface Vehiculo {
     id?: number,
     matricula?: string,
     bastidor?: string,
     modelo?: string;
 	marca?: string;
+    estadoReparacion?: EstadoReparacion;
 }
