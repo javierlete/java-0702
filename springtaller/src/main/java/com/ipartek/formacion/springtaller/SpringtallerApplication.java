150f64eda4919525d113d5dc6922d3f2e3840880
Javier Lete <javier@eskura21.com>
2025-11-03 16:27:04 +0100

    springtaller: Spring Boot/Starter: Lombok, Spring Data Jpa, Validation

---
+++ springtaller/src/main/java/com/ipartek/formacion/springtaller/SpringtallerApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springtaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtallerApplication.class, args);
+	}
+
+}
---
+++ springtaller/src/test/java/com/ipartek/formacion/springtaller/SpringtallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springtaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
