2b82bc278d15af59029b2e32bb150f5afa7c2528
Javier Lete <javier@eskura21.com>
2025-11-13 15:58:14 +0100

    Rol tiene ahora nombre único

--- springtaller/src/main/java/com/ipartek/formacion/springtaller/entidades/Rol.java
+++ springtaller/src/main/java/com/ipartek/formacion/springtaller/entidades/Rol.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,6 +34,7 @@ public class Rol {
 	
 	@NotBlank
 	@Size(max = 20)
+	@Column(unique = true)
 	private String nombre;
 	
 	@Size(max = 2000)
