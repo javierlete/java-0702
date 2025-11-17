05a512e80eddae7895514a4c8dcdf7b40ef7125e
Javier Lete <javier@eskura21.com>
2025-11-17 16:20:46 +0100

    ProductoRepository

---
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/repositorios/ProductoRepository.java
@@ -0,0 +1,12 @@
+package com.ipartek.formacion.amazonia.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ipartek.formacion.amazonia.modelos.Producto;
+
+@RepositoryRestResource(collectionResourceRel = "productos", path = "productos")
+public interface ProductoRepository extends CrudRepository<Producto, Long>, PagingAndSortingRepository<Producto, Long> {
+
+}
