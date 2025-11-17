35fd766dd054420499bd2e17102ed19f2ada25b2
Javier Lete <javier@eskura21.com>
2025-11-17 16:55:59 +0100

    Pruebas de repositorios y servicios

---
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/pruebas/Pruebas.java
@@ -0,0 +1,48 @@
+package com.ipartek.formacion.amazonia.pruebas;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import com.ipartek.formacion.amazonia.modelos.Producto;
+import com.ipartek.formacion.amazonia.repositorios.ProductoRepository;
+import com.ipartek.formacion.amazonia.servicios.AnonimoService;
+
+@Component
+public class Pruebas implements CommandLineRunner {
+	@Autowired
+	private ProductoRepository productoRepository;
+
+	@Autowired
+	private AnonimoService anonimoServicio;
+
+	@Override
+	public void run(String... args) throws Exception {
+		productoRepository.save(Producto.builder().nombre("Portátil").precio(new BigDecimal(1234)).build());
+		productoRepository.save(Producto.builder().nombre("Ratón").precio(new BigDecimal(12)).build());
+		productoRepository.save(Producto.builder().nombre("Teclado").precio(new BigDecimal(123)).build());
+
+		System.out.println("REPOSITORIO FINDALL");
+		productoRepository.findAll().forEach(System.out::println);
+
+		var pageable = Pageable.ofSize(2);
+
+		Page<Producto> pagina;
+
+		do {
+			pagina = anonimoServicio.listadoProductos(pageable);
+
+			if (pagina.hasContent()) {
+				System.out.println("PAGINA");
+				pagina.forEach(System.out::println);
+				
+				pageable = pagina.nextOrLastPageable();
+			}
+		} while (pagina.hasNext());
+	}
+
+}
