cc04f93cc395c22f8fb8acc7424523d8ab3cd3ca
Javier Lete <javier@eskura21.com>
2025-11-17 16:55:36 +0100

    AnonimoService

---
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/servicios/AnonimoService.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.amazonia.servicios;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.ipartek.formacion.amazonia.modelos.Producto;
+
+public interface AnonimoService {
+	Page<Producto> listadoProductos(Pageable pageable);
+}
---
+++ amazonia/src/main/java/com/ipartek/formacion/amazonia/servicios/AnonimoServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ipartek.formacion.amazonia.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.ipartek.formacion.amazonia.modelos.Producto;
+import com.ipartek.formacion.amazonia.repositorios.ProductoRepository;
+
+import lombok.extern.java.Log;
+
+@Log
+@Service
+public class AnonimoServiceImpl implements AnonimoService {
+	@Autowired
+	private ProductoRepository productoRepository;
+	
+	@Override
+	public Page<Producto> listadoProductos(Pageable pageable) {
+		log.info("Se han pedido los productos de la página " + pageable.getPageNumber() + " con tamaño " + pageable.getPageSize());
+		return productoRepository.findAll(pageable);
+	}
+	
+}
